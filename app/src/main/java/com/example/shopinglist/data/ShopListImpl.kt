f7111db6a7d794617ae7ef51e2e06f718008e914
Fedor <bysov1987@mail.ru>
2022-10-15 02:27:42 +0300

    list displayed on the screen

--- app/src/main/java/com/example/shopinglist/data/ShopListImpl.kt
+++ app/src/main/java/com/example/shopinglist/data/ShopListImpl.kt
@@ -9,7 +9,7 @@ import com.example.shopinglist.domain.ShopListRepository
 object ShopListImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({t1 , t2 -> t1.id.compareTo(t2.id)})
 
     private var autoDefinitionId = 0
 
@@ -27,11 +27,13 @@ object ShopListImpl : ShopListRepository {
         }
 
         shopList.add(shopItem)
+        updateList()
 
     }
 
     override fun deleteItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopList(shopItem: ShopItem) {
