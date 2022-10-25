5351b38e9f1f05e2d4db8d31262477ba2eea2dc2
Fedor <bysov1987@mail.ru>
2022-10-25 22:20:34 +0300

    Working version of the application

--- app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         val buttonAddItem = binding.btnAdd
         buttonAddItem.setOnClickListener {
             val intent = ShopItemActivity.newIntentShopItemActivity(this)
+
             startActivity(intent)
         }
 
