ecc292b782f222ead13b68026b553abd569ecfe8
Fedor <bysov1987@mail.ru>
2022-10-26 01:33:11 +0300

    migration_to_fragment

--- app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
@@ -23,21 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-        val buttonAddItem = binding.btnAdd
-        buttonAddItem.setOnClickListener {
-            val intent = ShopItemActivity.newIntentShopItemActivity(this)
-
-            startActivity(intent)
-        }
-
-        setupAdapter()
-        setupLongClick()
-
-        setupClick()
-
-        setupItemTouchHelper()
-
-
         viewModel.shopList.observe(this) {
             Log.d("TEST_LOADING_LIVE_DATA", "${it.toString()}")
             mainAdapter.submitList(it)
@@ -46,75 +31,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun setupAdapter() {
-        mainAdapter = AdapterMainActivity()
-        binding.listRV.adapter = mainAdapter
-        binding.listRV.recycledViewPool.setMaxRecycledViews(
-            AdapterMainActivity.ENABLED_VIEW,
-            AdapterMainActivity.POOL_VIEW_ACTIV
-        )
-        binding.listRV.recycledViewPool.setMaxRecycledViews(
-            AdapterMainActivity.DISABLED_VIEW,
-            AdapterMainActivity.POOL_VIEW_DISABLE
-        )
-    }
-
-    private fun setupItemTouchHelper() {
-        val callback = object : ItemTouchHelper.SimpleCallback(
-            0,
-            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val item = mainAdapter.currentList[viewHolder.adapterPosition]
-                viewModel.deleteShopList(item)
-            }
-        }
-        val itemTouchHelper = ItemTouchHelper(callback)
-        itemTouchHelper.attachToRecyclerView(binding.listRV)
-    }
-
-    private fun setupClick() {
-        mainAdapter.onShopItemCoinCliClickListener = {
-            Log.d("onShopItemCoinCliClick", " ${it.toString()}")
-            val intent = ShopItemActivity.editIntentShopItemActivity(this, it.id)
-            startActivity(intent)
-        }
-    }
-
-    private fun setupLongClick() {
-        mainAdapter.onShopItemCoinLongCliClickListener = {
-            viewModel.changeEnabledList(it)
-        }
-    }
-
-
-//    fun showList(list: List<ShopItem>) {
-//        binding.llShopList.removeAllViews()
-//        for (shopItem in list) {
-//            val layoutId = if (shopItem.enabled) {
-//                R.layout.item_note_info
-//            } else {
-//                R.layout.item_hind_note_info
-//            }
-//            val view = LayoutInflater.from(this).inflate(layoutId, linearLayout, false)
-//            val text = view.findViewById<TextView>(R.id.textNoteTV)
-//            val count = view.findViewById<TextView>(R.id.countTV)
-//            count.text = shopItem.count.toString()
-//            text.text = shopItem.name
-//            view.setOnLongClickListener {
-//                viewModel.changeEnabledList(shopItem)
-//                true
-//            }
-//            binding.llShopList.addView(view)
-//
-//        }
-//    }
 }
